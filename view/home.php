9905a2bafb84013e5b9f5ad55b865ba8d632e0b1
TimZ <timour1717@gmail.com>
2022-02-13 00:45:35 +0100

    Initial commit

---
+++ view/home.php
@@ -0,0 +1,28 @@
+
+
+<div id = "titil_one" class="row">
+    <div class="column">
+        <right_div class="right_div">
+
+            <p>
+                Association de bienfaisance
+            </p>
+            <h1 class="">
+                INITIATIVE HUMANITAIRE INTERNATIONALE
+            </h1>
+
+
+            <button id = "button_one" type="button" class="btn btn-primary ">Déposer une demande</button>
+        </right_div>
+    </div>
+
+    <div class="column">
+        <left_div>
+            <img src="palm_of_hand_6.png" width="300" height="200">
+        </left_div>
+    </div>
+</div>
+
+
+
+</container>
