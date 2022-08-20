253989c8a213194912b74f8d3738e2f948d556bd
John Doe <timour1712@gmail.com>
2022-08-21 01:07:55 +0200

    sqdf

--- view/explanation.php
+++ view/explanation.php
@@ -49,8 +49,8 @@
 </container>
 </container_main>
 </body>
-<script type='text/javascript' src='css_js/js/popper.min.js'></script>
-<script type='text/javascript' src='css_js/js/bootstrap.js'></script>
-<script type='text/javascript' src='css_js/js/JavaScript.js'></script>
+<script type='text/javascript' src='../css_js/js/popper.min.js'></script>
+<script type='text/javascript' src='../css_js/js/bootstrap.js'></script>
+<script type='text/javascript' src='../css_js/js/JavaScript.js'></script>
 
 </html>
--- view/heder_pls.php
+++ view/heder_pls.php
@@ -1,140 +1,139 @@
 <!-- http://www.w3.org/2000/svg -->
 <svg xmlns="" style="display: none;">
-        <symbol id="bootstrap" viewBox="0 0 118 94">
-            <title>Bootstrap</title>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
-        </symbol>
-        <symbol id="facebook" viewBox="0 0 16 16">
-            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
-        </symbol>
-        <!-- <symbol id="instagram" viewBox="0 0 16 16">
+    <symbol id="bootstrap" viewBox="0 0 118 94">
+        <title>Bootstrap</title>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
+    </symbol>
+    <symbol id="facebook" viewBox="0 0 16 16">
+        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+    </symbol>
+    <!-- <symbol id="instagram" viewBox="0 0 16 16">
             <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
         </symbol> -->
-        <symbol id="twitter" viewBox="0 0 16 16">
-            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
-        </symbol>
+    <symbol id="twitter" viewBox="0 0 16 16">
+        <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+    </symbol>
 
-        <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-1">
-                <stop stop-color="#38AEEB" offset="0%" />
-                <stop stop-color="#279AD1" offset="100%" />
-            </linearGradient>
-        </defs>
-        <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <circle id="Oval" fill="url(#linearGradient-1)" cx="8" cy="8" r="8" />
-            <path d="M3.17026167,7.83635602 C5.78750201,6.74265999 7.53273882,6.02162863 8.40597211,5.67326193 C10.8992306,4.67860423 11.2454541,4.53439191 11.5831299,4.52864956 C11.6573986,4.52743168 11.8385417,4.55776042 11.9798438,4.67645833 C12.1211458,4.79515625 12.1635786,4.87206678 12.1755371,4.93908691 C12.1874957,5.00610705 12.1862759,5.21456762 12.1744385,5.3338623 C12.0393279,6.69547283 11.5259342,9.83829771 11.2285121,11.3633248 C11.1026617,12.008621 10.8548582,12.2249854 10.6149558,12.2461596 C10.0935924,12.2921758 9.69769267,11.9156852 9.19272668,11.5981993 C8.40255458,11.1013965 8.13911734,10.9180161 7.3721185,10.4332283 C6.48571864,9.87297217 6.85080034,9.6784879 7.35595703,9.17524981 C7.48815894,9.04355001 9.67825076,7.04590073 9.71077046,6.86250183 C9.7391276,6.70257812 9.7494847,6.68189389 9.67664063,6.60973958 C9.60379655,6.53758527 9.51674192,6.54658941 9.46083149,6.55876051 C9.38158015,6.57601267 8.17521836,7.33962686 5.84174612,8.84960308 C5.48344358,9.08558775 5.15890428,9.20056741 4.86812819,9.19454205 C4.54757089,9.18789957 3.93094724,9.02070014 3.47255094,8.87778221 C2.91030922,8.70248755 2.46345069,8.609808 2.50236203,8.31210343 C2.52262946,8.15704047 2.74526267,7.998458 3.17026167,7.83635602 Z" id="Path-3" fill="#FFFFFF" />
-        </g>
-    </svg>
+    <defs>
+        <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-1">
+            <stop stop-color="#38AEEB" offset="0%" />
+            <stop stop-color="#279AD1" offset="100%" />
+        </linearGradient>
+    </defs>
+    <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <circle id="Oval" fill="url(#linearGradient-1)" cx="8" cy="8" r="8" />
+        <path d="M3.17026167,7.83635602 C5.78750201,6.74265999 7.53273882,6.02162863 8.40597211,5.67326193 C10.8992306,4.67860423 11.2454541,4.53439191 11.5831299,4.52864956 C11.6573986,4.52743168 11.8385417,4.55776042 11.9798438,4.67645833 C12.1211458,4.79515625 12.1635786,4.87206678 12.1755371,4.93908691 C12.1874957,5.00610705 12.1862759,5.21456762 12.1744385,5.3338623 C12.0393279,6.69547283 11.5259342,9.83829771 11.2285121,11.3633248 C11.1026617,12.008621 10.8548582,12.2249854 10.6149558,12.2461596 C10.0935924,12.2921758 9.69769267,11.9156852 9.19272668,11.5981993 C8.40255458,11.1013965 8.13911734,10.9180161 7.3721185,10.4332283 C6.48571864,9.87297217 6.85080034,9.6784879 7.35595703,9.17524981 C7.48815894,9.04355001 9.67825076,7.04590073 9.71077046,6.86250183 C9.7391276,6.70257812 9.7494847,6.68189389 9.67664063,6.60973958 C9.60379655,6.53758527 9.51674192,6.54658941 9.46083149,6.55876051 C9.38158015,6.57601267 8.17521836,7.33962686 5.84174612,8.84960308 C5.48344358,9.08558775 5.15890428,9.20056741 4.86812819,9.19454205 C4.54757089,9.18789957 3.93094724,9.02070014 3.47255094,8.87778221 C2.91030922,8.70248755 2.46345069,8.609808 2.50236203,8.31210343 C2.52262946,8.15704047 2.74526267,7.998458 3.17026167,7.83635602 Z" id="Path-3" fill="#FFFFFF" />
+    </g>
+</svg>
 
-    <container_main class="container">
-        <span class="screen-darken"></span>
+<container_main class="container">
+    <span class="screen-darken"></span>
 
-        <!-- Ninja nav-bar at the top, ninja means it adapts to mobile -->
+    <!-- Ninja nav-bar at the top, ninja means it adapts to mobile -->
 
-        <nav id="navbar_top" class="mobile-offcanvas navbar navbar-expand-lg navbar-light    bg-light border-bottom">
-            <div class="container-fluid hstack gap-5">
-                <div class="offcanvas-header">
-                    <button class="btn-close float-end btn-xl-right "></button>
-                </div>
+    <nav id="navbar_top" class="mobile-offcanvas navbar navbar-expand-lg navbar-light    bg-light border-bottom">
+        <div class="container-fluid hstack gap-5">
+            <div class="offcanvas-header">
+                <button class="btn-close float-end btn-xl-right "></button>
+            </div>
 
-                <div class="bg-light col-md-1">
-                    <!--Left space (col-md-1) automatically pushes to the right until equilibrium with the 2 ms-auto on the right-->
-                </div>
-                <ul class="navbar-nav">
+            <div class="bg-light col-md-1">
+                <!--Left space (col-md-1) automatically pushes to the right until equilibrium with the 2 ms-auto on the right-->
+            </div>
+            <ul class="navbar-nav">
 
 
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> IHI </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item go_to_section" href="?page=home#quiSN"> Qui sommes nous </a></li>
-                            <li><a class="dropdown-item go_to_section" href="?page=home#progH">Programmes </a></li>
-                            <li><a class="dropdown-item go_to_section" href="?page=home#ofs">Office </a></li>
-                            <li><a class="dropdown-item go_to_section" href="?page=home#contact">Contactez nous </a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> IHI </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item go_to_section" href="?page=home#quiSN"> Qui sommes nous </a></li>
+                        <li><a class="dropdown-item go_to_section" href="?page=home#progH">Programmes </a></li>
+                        <li><a class="dropdown-item go_to_section" href="?page=home#ofs">Office </a></li>
+                        <li><a class="dropdown-item go_to_section" href="?page=home#contact">Contactez nous </a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Donateurs </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#"> Mécènes </a></li>
+                        <li><a class="dropdown-item" href="#"> Sponsors </a></li>
+
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Bénévoles </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href=".?page=explanation"> Règlement sur les bénévoles</a></li>
+                        <!-- href="y_storage/Règlement sur les bénévoles.pdf" -->
+                        <li><a class="dropdown-item" href=".?page=login"> Devenir bénevole </a></li>
+
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown">Nos missions </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href=""> Règlement sur les missions</a></li>
+                        <li><a class="dropdown-item" href=""> Liste de missions </a></li>
+
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> FAQ </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#"> </a></li>
+                        <li><a class="dropdown-item" href="#"> </a></li>
+
+                    </ul>
+                </li>
+            </ul>
+
+
+
+            <ul class="navbar-nav ms-auto">
+                <!--right space (ms-auto) automatically pushes to the left until equilibrium with the 1st ms-auto on the left-->
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2 small">English</a></li>
+            </ul>
+
+            <ul class="navbar-nav hid_btn_xl">
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <button href="index.php?page=connexion" class="nav-link  dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Connexion
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href=".?page=login">Entrer</a></li>
+                            <li><a class="dropdown-item" href=".?page=register">Créer mon compte</a></li>
+
                         </ul>
-                    </li>
-                </ul>
+                    </div>
+                </li>
+            </ul>
 
-                <ul class="navbar-nav">
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Donateurs </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#"> Mécènes </a></li>
-                            <li><a class="dropdown-item" href="#"> Sponsors </a></li>
-
-                        </ul>
-                    </li>
-                </ul>
-
-                <ul class="navbar-nav">
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Bénévoles </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item"  href="y_storage/Règlement sur les bénévoles.pdf"> Règlement sur les bénévoles</a></li> 
-                            <!--  --> 
-                            <li><a class="dropdown-item" href=".?page=login"> Devenir bénevole </a></li>
-
-                        </ul>
-                    </li>
-                </ul>
-
-                 <ul class="navbar-nav">
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Bénévoles </a>
-                        <ul class="dropdown-menu">
-                             <li><a class="dropdown-item" href="y_storage/Règlement sur les bénévoles.pdf" > Règlement sur les bénévoles</a></li> 
-                              <!--href="y_storage/Règlement sur les bénévoles.pdf" -->
-                            <li><a class="dropdown-item" href=".?page=login"> Devenir bénevole </a></li>
-
-                        </ul>
-                    </li>
-                </ul>
-
-                <ul class="navbar-nav">
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> FAQ </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#"> </a></li>
-                            <li><a class="dropdown-item" href="#"> </a></li>
-
-                        </ul>
-                    </li>
-                </ul>
+        </div> <!-- container-fluid.// -->
+    </nav>
+    <button data-trigger="navbar_top" id="button_menu" class="d-lg-none btn btn-warning beige btn-xl" type="button"> Menu </button>
 
 
+    <!-- end  First nav-bar with links and language -->
 
-                <ul class="navbar-nav ms-auto">
-                    <!--right space (ms-auto) automatically pushes to the left until equilibrium with the 1st ms-auto on the left-->
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2 small">English</a></li>
-                </ul>
-
-                <ul class="navbar-nav hid_btn_xl">
-                    <li class="nav-item">
-                        <div class="dropdown">
-                            <button href="index.php?page=connexion" class="nav-link  dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                Connexion
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="dropdown-item" href=".?page=login">Entrer</a></li>
-                                <li><a class="dropdown-item" href=".?page=register">Créer mon compte</a></li>
-
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-
-            </div> <!-- container-fluid.// -->
-        </nav>
-        <button data-trigger="navbar_top" id="button_menu" class="d-lg-none btn btn-warning beige btn-xl" type="button"> Menu </button>
-
-
-        <!-- end  First nav-bar with links and language -->
-
-        <?php
-        if ($_SESSION['page'] == 'home') {
-            include "header_suite.php";
-        }
+    <?php
+    if ($_SESSION['page'] == 'home') {
+        include "header_suite.php";
+    }
