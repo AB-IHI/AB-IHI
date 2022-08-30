15ce95c4ef7fa62539630b0d99aeee208d19b7ea
John Doe <timour1712@gmail.com>
2022-08-30 16:43:35 +0200

    improved mobile view for explanation

---
+++ explanation.php
@@ -0,0 +1,201 @@
+<!-- not in the view because it's a simple text page w links -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>IHI</title>
+
+    <link rel="stylesheet" type="text/css" href="css_js/bootstrap.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="css_js/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/> -->
+
+    <link rel="stylesheet" type="text/css" href="css_js/style.css" />
+
+
+</head>
+
+<body class="mb-0">
+
+
+
+    <container_main class="container">
+
+    <container class="auto-mx don">
+
+<div id="" class="mobile-offcanvas d-flex align-items-center justify-content-center">
+    <?php include_once "Règlement bref sur les  bénévole.html"; ?>
+</div>
+
+</container>
+
+<container class="auto-mx hid_btn_xl">
+
+<article class="article">
+    <div class="container px-4 py-5">
+        <div id="" class="d-flex align-items-center justify-content-center">
+            <a id="a_​_Voir_" href="y_storage/Règlement sur les bénévoles.pdf"><div class="T2">Voir+</div></a>
+            <p class="T4 Tcustom pb-2 border-bottom ">Devenir bénévole </p>
+        
+        <div class="">
+            <div class="wrapper ">
+                <p>
+                    Toute personne peut participer aux activités de l'Association en tant que bénévole si elle partage les idées d'humanisme et est prête à appliquer ses efforts, ses connaissances et son expérience pour assurer la mise en œuvre réussie de programmes humanitaires et de projets caritatifs de l'Association, et si elle reconnaît et suit également les règles énoncées dans le présent Règlement.
+            
+                    Pour rejoindre les rangs des bénévoles, il est nécessaire d'enregistrer la candidature correspondante sur le site internet de l'Association. A l'issue de son inscription, le candidat reçoit une invitation à un entretien et l'accès aux informations sur les missions humanitaires de l'Association en tant que volontaire. A l'issue de la période d'essai, le candidat reçoit le statut de bénévole de l'Association.
+            
+                    La présence de bénévoles impose à l'Association des responsabilités et des obligations tant envers les bénévoles eux-mêmes que pour les actions de chaque bénévole. Afin de réduire les divers risques et d'indemniser les dommages éventuels, l'Association interagit avec les institutions d'assurance, les autorités publiques et indépendantes de la régulation.
+            
+                    L'Association tient des registres des bénévoles, organise et crée des conditions sûres et confortables pour leur permettre de participer à des missions humanitaires.
+                </p>
+            </div>
+        </div>
+    </div>
+</article>
+
+     
+     
+     
+
+<article class="article">
+    <div class="container px-4 py-5" id="featured-3">
+        <div class="d-flex align-items-center justify-content-center">
+            <p class="T4 pb-2 border-bottom h2">Catégories spéciales de bénévoles</p>
+            
+            <div class="row-p g-4 py-1 row-cols-1 row-cols-lg-1">
+                <div class="wrapper feature col">
+                    <p>
+                        Un chômeur peut se porter bénévole pour l’Association sans affecter son droit à percevoir des allocations de chômage. Dans ce cas, certaines conditions doivent être respectées :
+                
+                            il n'a pas travaillé auparavant comme employé de l'Association ;
+                
+                            ses activités bénévoles :
+                
+                            ne doivent pas interférer avec sa recherche active d'emploi,
+                
+                            ne doivent pas remplacer le travail rémunéré.
+                
+                        Attirer les employés vers des activités bénévoles est facilité par certains mécanismes organisés de l'entreprise dans laquelle ils travaillent. Ils peuvent également participer au mouvement bénévole avec l'entreprise de deux manières :
+                
+                            bénévolat de compétences : l'employeur facilite les échanges entre ses salariés et l'Association, les salariés qui le souhaitent font du bénévolat de leur temps personnel, et l'Association facilite l'accès de ces bénévoles à des missions de volontariat spécifiques compatibles avec les journées de travail ;
+                
+                            mécénat de compétences : l'employeur invite ses salariés à consacrer quelques heures de leur temps de travail à un projet commun avec l'Association ; l'entreprise peut bénéficier d'une réduction d'impôt en fonction du salaire d'un bénévole durant sa mission au sein de l'Association.
+                
+                        Sous certaines conditions, les salariés peuvent bénéficier d'un congé afin d'investir leur temps personnel dans une vie associative (voir chapitre 10. Les congés de bénévolat).
+                
+                        Une personne en âge de préretraite peut facilement devenir bénévole dans une association, à la seule condition que ce bénévolat ne se fasse pas dans la structure dans laquelle elle était auparavant salarié.
+                
+                        Références juridiques :
+                
+                        Des chômeurs : code du travail, article L 5425-8
+                
+                        Des préretraités : code du travail, art. R. 5123-18
+                
+                        En savoir + :
+                
+                        
+                    </p>
+                    <p>
+                        -<a href="https://www.associations.gouv.fr/cumul-des-statuts-etre-benevole-et-en-preretraite.html
+                                        ">Être bénévole et en préretraite </a>
+                            <pre>
+<p>-</p><a href="https://www.associations.gouv.fr/je-suis-actif.html">actifs</a>
+<p>-</p><a href="https://www.associations.gouv.fr/je-suis-senior.html">préretraités</a>
+                            </pre>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</article>
+
+    
+    
+   <article class="article">
+       <div class="container px-4 py-5">
+           <div class="d-flex align-items-center justify-content-center">
+               <a id="a_​_Voir_" href="/y_storage/Règlement sur les bénévoles.pdf"><div class="T2">Voir+</div></a>
+               <p class="T4">Droits du bénévole</p>
+               <div class="row-p g-4 py-1 row-cols-1 row-cols-lg-1">
+                   <div class="wrapper">
+                    <p>
+                   
+                        Le bénévole a le droit :
+                   
+                            de choisir en toute autonomie la mission humanitaire au sein de laquelle il souhaite exercer ses activités ;
+                            sans aucune démarche ni indemnité, de résilier ou suspendre sa participation à une mission humanitaire, ainsi que son statut en tant que bénévole de l'Association ;
+                            prendre en toute indépendance des décisions dans le cadre de l'exécution d'une mission humanitaire dans le cadre des conditions et restrictions prévues par le présent Règlement, les recommandations contenues dans la description de chaque programme humanitaire spécifique ou projet caritatif, ainsi que les instructions des responsables des groupes de bénévoles ou du directeur d’Office ;
+                            aux conditions sûres et confortables pour mener à bien ses activités dans le cadre des missions humanitaires de l'Association ;
+                            de contrôler les activités des organes de gestion de l'Association en comparant les données accessibles au public avec l'état actuel des choses, ainsi qu'en utilisant d'autres méthodes légitimes de vérification ;
+                            de formuler des suggestions et des commentaires aux responsables des groupes de bénévoles, à la direction de l’Office et de l'Association, visant à améliorer l'efficacité des missions humanitaires, les activités des bénévoles qui y participent, la direction des Offices et de l'Association ;
+                            de former une culture des relations afin d'améliorer l'interaction et la compréhension mutuelle dans l'équipe à tous les niveaux structurels de l'Association en initiant des procédures et des technologies appropriées.
+                    </p>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </article>
+
+ 
+
+     
+   <article class="article">
+       <div class="container px-4 py-5">
+           <div class="d-flex align-items-center justify-content-center">
+    <a id="a_​_Voir_" href="/y_storage/Règlement sur les bénévoles.pdf"><div class="T2">Voir+</div></a> 
+    <p class="T4">Devoirs du bénévole</p>
+<div class="wrapper">
+    <p>
+        
+        Le bénévole n'est soumis à aucun lien de subordination juridique.
+        
+        Cependant, la participation de chaque bénévole aux activités de l'Association implique l'accomplissement bénévole d'un certain ensemble de devoirs, parmi lesquels l'interaction avec d'autres bénévoles, la capacité de trouver son rôle dans l'équipe, et de faire preuve de bienveillance et de courtoisie envers toutes les personnes qui l'entourent.
+        
+        L'Association attend de chaque bénévole impliqué dans la mise en œuvre d’une mission humanitaire :
+        
+            à la fois la motivation et l'initiative dans la mise en œuvre des missions humanitaires, ainsi qu'une auto-organisation et une rigueur maximales en matière d'assistance aux bénéficiaires, car la santé et, dans certains cas, la vie des personnes dépendent de la décision et de l'assistance opportune ;
+            la prudence et le soin dans leurs actions, puisqu'il s'agit de la confiance des mécènes et sponsors qui ont confié à l'Association le transfert de valeurs matérielles, l'apport d'un soutien ou d'un service aux personnes qui en ont besoin ;
+            la capacité de prévoir et d'éviter divers types de risques tant pour soi personnellement que pour tous les participants aux missions humanitaires, en suivant les instructions du présent Règlement, les recommandations contenues dans la description de chaque programme humanitaire ou projet caritatif spécifique, ainsi que les instructions des responsables de groupes de bénévoles ou du directeur d’Office ;
+            être extrêmement objectif dans l'évaluation de la situation de vie des personnes, de leurs besoins et de leurs problèmes.
+        
+        L'incompatibilité des qualités personnelles d'un bénévole avec les conditions énumérées ci-dessus ou son refus de répondre aux exigences du rôle qui lui est confié dans la mission humanitaire rend impossible de continuer à y participer, et dans certains cas remet en cause  sa poursuite du séjour dans le statut de bénévole de l'Association.
+        
+        Il est important de comprendre qu'en cas de déviation aux exigences de la mission humanitaire, le bénévole est personnellement responsable des éventuelles conséquences négatives.
+    </p>
+</div>
+
+    <p class="T2"><a id="a_​_Voir_" href="y_storage/Règlement sur les bénévoles.pdf"><div class="T2">Voir+</div></a></p> 
+    </p>
+
+</div>
+       </div>
+   </article>
+
+</container>
+
+</div>
+
+
+
+<!-- footer -->
+<span class="spacer">
+</span>
+<container id="footer_" class="container sticky-bottom my-auto">
+    <footer class="pt-4 mt-4 footer mt-auto sticky-bottom">
+
+
+
+        <p class="text-center text-muted ">
+            <!--hidden when full width -->
+            © 2022 IHI asso
+        </p>
+
+    </footer>
+
+</container>
+</container_main>
+</body>
+<script type='text/javascript' src='css_js/js/popper.min.js'></script>
+<script type='text/javascript' src='css_js/js/bootstrap.js'></script>
+<script type='text/javascript' src='css_js/js/JavaScript.js'></script>
+
+</html>
