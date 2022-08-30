9630ab854ea2b918a97c5dce31c2479fe82dea7d
John Doe <timour1712@gmail.com>
2022-08-30 17:01:52 +0200

    again mobile of explanation

--- view/headerr.php
+++ view/headerr.php
@@ -1,28 +1,15 @@
+<!-- useless -->
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8">
-    <title>IHI</title>
+    <title>volunteer</title>
 
-    <link rel="stylesheet" type="text/css" href="css_js/bootstrap.css" />
-    <!-- <link rel="stylesheet" type="text/css" href="css_js/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/> -->
+    <link rel="stylesheet" type="text/css" href="css_js/css/bootstrap.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="css_js/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>  -->
 
-    <link rel="stylesheet" type="text/css" href="css_js/style.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="css_js/style.css" /> -->
 
 
 </head>
-
-<body class="mb-0">
-
-
-
-    <container_main class="container">
-
-    <container class="auto-mx">
-
-<div id="title_one" class="d-flex align-items-center justify-content-center">
-    <h1 id="title_two">Volunteer's personal page of "Shining of the hope"</h1>
-</div>
-
-</container>
