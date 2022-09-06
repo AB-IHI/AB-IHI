178155a508e511cfc2335e0ed49b6da39f30a973
John Doe <timour1712@gmail.com>
2022-09-06 19:28:54 +0200

    sdf

--- view/inside_nav.php
+++ view/inside_nav.php
@@ -2,74 +2,77 @@
 
 
 
-<li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> IHI </a>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item go_to_section" href="#quiSN"> Qui sommes nous </a></li>
-        <li><a class="dropdown-item go_to_section" href="#progH">Programmes </a></li>
-        <li><a class="dropdown-item go_to_section" href="#ofs">Office </a></li>
-        <li><a class="dropdown-item go_to_section" href="#contact">Contactez nous </a></li>
-    </ul>
-</li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> IHI </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item go_to_section" href="#quiSN"> Qui sommes nous </a></li>
+            <li><a class="dropdown-item go_to_section" href="#progH">Programmes </a></li>
+            <li><a class="dropdown-item go_to_section" href="#ofs">Office </a></li>
+            <li><a class="dropdown-item go_to_section" href="#contact">Contactez nous </a></li>
+        </ul>
+    </li>
 </ul>
 
 <ul class="navbar-nav">
 
-<li class="nav-item dropdown">
-    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Parrainage </a>
-    <ul class="dropdown-menu">
-        
-        <li><a class="dropdown-item" href=".?explain=parrains"> Parrain </a></li>
+    <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown">Nos missions </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href=".?explain=act_bienfsce"> Règlement sur les missions</a></li>
+            <li><a class="dropdown-item" href="#"> L'aube de l'espoir</a></li>
 
-    </ul>
-</li>
+        </ul>
+    </li>
 </ul>
 
 <ul class="navbar-nav">
 
-<li class="nav-item dropdown">
-    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Bénévoles </a>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href=".?explain=explanation"> Règlement sur les bénévoles</a></li>
-        <li><a class="dropdown-item" href=".?page=login"> Devenir bénevole </a></li>
-
-    </ul>
-</li>
+    <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Mécénat </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href=".?explain=mecenes">Mècènes </a></li>
+            <li><a class="dropdown-item" href=".?explain=fisc_mecenat">Le régime fiscal </a></li>
+            <li><a class="dropdown-item" href=".?explain=explain_don_org_foreign">Organismes étrangers </a></li>
+        </ul>
+    </li>
 </ul>
 
 <ul class="navbar-nav">
 
-<li class="nav-item dropdown">
-    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown">Nos missions </a>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href=".?explain=act_bienfsce"> Règlement sur les missions</a></li>
-        <li><a class="dropdown-item" href="#"> L'aube de l'espoir</a></li>
+    <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Parrainage </a>
+        <ul class="dropdown-menu">
 
-    </ul>
-</li>
-</ul>
+            <li><a class="dropdown-item" href=".?explain=parrains"> Parrain </a></li>
 
-
-<ul class="navbar-nav">
-
-<li class="nav-item dropdown">
-    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> FAQ </a>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#"> </a></li>
-        <li><a class="dropdown-item" href="#"> </a></li>
-
-    </ul>
-</li>
+        </ul>
+    </li>
 </ul>
 
 <ul class="navbar-nav">
 
-<li class="nav-item dropdown">
-    <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Mécénat </a>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href=".?explain=mecenes">Mècènes </a></li>
-        <li><a class="dropdown-item" href=".?explain=fisc_mecenat">Le régime fiscal </a></li>
-        <li><a class="dropdown-item" href=".?explain=explain_don_org_foreign">Organismes étrangers </a></li>
-    </ul>
-</li>
+    <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> Bénévoles </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href=".?explain=explanation"> Règlement sur les bénévoles</a></li>
+            <li><a class="dropdown-item" href=".?page=login"> Devenir bénevole </a></li>
+
+        </ul>
+    </li>
 </ul>
+
+
+
+
+<ul class="navbar-nav">
+
+    <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-bs-toggle="dropdown"> FAQ </a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#"> </a></li>
+            <li><a class="dropdown-item" href="#"> </a></li>
+
+        </ul>
+    </li>
+</ul>
+
