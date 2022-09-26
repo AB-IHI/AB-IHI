cb4fcfd508d4949ca34e5fcc86506a71500875eb
John Doe <timour1712@gmail.com>
2022-09-27 00:10:45 +0200

    added new files

---
+++ lau.php
@@ -0,0 +1,123 @@
+<!-- not in the view because it's a simple text page w links -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>IHI</title>
+
+    <!-- <link rel="stylesheet" type="text/css" href="css_js/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css_js/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/> -->
+    <style type="text/css">
+
+@page {  }
+    table { border-collapse:collapse; border-spacing:0; empty-cells:show }
+    td, th { vertical-align:top; font-size:12pt;}
+    h1, h2, h3, h4, h5, h6 { clear:both;}
+    ol, ul { margin:0; padding:0;}
+    li { list-style: none; margin:0; padding:0;}
+    /* "li span.odfLiEnd" - IE 7 issue*/
+    li span. { clear: both; line-height:0; width:0; height:0; margin:0; padding:0; }
+    span.footnodeNumber { padding-right:1em; }
+    span.annotation_style_by_filter { font-size:95%; font-family:Arial; background-color:#fff000;  margin:0; border:0; padding:0;  }
+    span.heading_numbering { margin-right: 0.8rem; }* { margin:0;}
+    .P1 { color:#000000; font-size:12pt; margin-bottom:0cm; margin-top:0cm; text-align:center ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; }
+    .P10 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; margin-left:0.6cm; margin-right:0cm; text-indent:-0.6cm; }
+    .P11 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.499cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; }
+    .P12 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; }
+    .P13 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; }
+    .P3 { color:#000000; font-size:18pt; font-weight:bold; margin-bottom:0.212cm; margin-top:0.423cm; text-align:left ! important; font-family:Liberation Sans; writing-mode:horizontal-tb; direction:ltr; line-height:100%; }
+    .P4 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; }
+    .P5_borderStart { color:#00a933; font-size:12pt; line-height:150%; margin-top:0cm; text-align:left ! important; font-family:Tahoma; writing-mode:horizontal-tb; direction:ltr; font-weight:bold; background-color:#ffffff; padding-bottom:0.247cm;  border-bottom-style:none; }
+    .P5 { color:#00a933; font-size:12pt; line-height:150%; text-align:left ! important; font-family:Tahoma; writing-mode:horizontal-tb; direction:ltr; font-weight:bold; background-color:#ffffff; padding-bottom:0.247cm; padding-top:0cm;  border-top-style:none; border-bottom-style:none; }
+    .P5_borderEnd { color:#00a933; font-size:12pt; line-height:150%; margin-bottom:0.247cm; text-align:left ! important; font-family:Tahoma; writing-mode:horizontal-tb; direction:ltr; font-weight:bold; background-color:#ffffff; padding-top:0cm;  border-top-style:none;}
+    .P6 { color:#000000; font-size:12pt; line-height:100%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; }
+    .P7 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; font-weight:bold; }
+    .P8_borderStart { color:#000000; font-size:12pt; line-height:150%; margin-top:0cm; text-align:left ! important; font-family:Tahoma; writing-mode:horizontal-tb; direction:ltr; margin-left:0cm; margin-right:0cm; text-indent:0cm; background-color:#ffffff; padding-bottom:0.247cm;  border-bottom-style:none; }
+    .P8 { color:#000000; font-size:12pt; line-height:150%; text-align:left ! important; font-family:Tahoma; writing-mode:horizontal-tb; direction:ltr; margin-left:0cm; margin-right:0cm; text-indent:0cm; background-color:#ffffff; padding-bottom:0.247cm; padding-top:0cm;  border-top-style:none; border-bottom-style:none; }
+    .P8_borderEnd { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; text-align:left ! important; font-family:Tahoma; writing-mode:horizontal-tb; direction:ltr; margin-left:0cm; margin-right:0cm; text-indent:0cm; background-color:#ffffff; padding-top:0cm;  border-top-style:none;}
+    .P9 { color:#000000; font-size:12pt; line-height:150%; margin-bottom:0.247cm; margin-top:0cm; text-align:left ! important; font-family:Liberation Serif; writing-mode:horizontal-tb; direction:ltr; margin-left:0cm; margin-right:0cm; text-indent:0cm; }
+    .T2 { color:#00a933; font-family:Tahoma; font-size:12pt; font-weight:bold; background-color:#ffffff; }
+    .T3 { font-family:Tahoma; font-size:18pt; font-weight:normal; background-color:#ffffff; }
+    .T4 { color:#2a6099; font-size:40pt; background-color:#ffffff; }
+    .T5 { color:#2a6099; font-family:Tahoma; font-size:20pt; font-weight:bold; background-color:#ffffff; }
+    .T6 { color:#ea7500; font-family:Tahoma; font-size:18pt; font-weight:bold; background-color:#ffffff; }
+    /* ODF styles with no properties representable as CSS */
+    .T1  { }</style>
+    <link rel="stylesheet" type="text/css" href="css_js/expln°style.css" />
+
+
+</head>
+
+<body class="mb-0">
+
+
+
+    <container_main class="container">
+
+    <container class="auto-mx ">
+
+<div id="" class=" d-flex align-items-center justify-content-center">
+    <?php 
+    include_once "01. L'aube de l'espoir.html"; 
+    ?>
+</div>
+
+</container>
+
+<container class="auto-mx hid_btn_xl">
+<!-- 
+<article class="article">
+    <div class="container px-4 py-5">
+        <div id="featured" class="d-flex align-items-center justify-content-center">
+            <a id="a_​_Voir_" href="y_storage/Règlement sur les bénévoles.pdf"><div class="T2 Tcustom">Voir+</div></a>
+            <h1 class="T4 Tcustom pb-2 border-bottom ">​ Dons faits au profit d'organismes étrangers   </h1>
+        
+        <div class="">
+            <div class="wrapper ">
+                <p>
+                
+    
+
+
+                </p>
+            </div>
+        </div>
+    </div>
+</article> -->
+
+     
+     
+     <!-- end -->
+
+
+
+</container>
+
+</div>
+
+
+
+<!-- footer -->
+<span class="spacer">
+</span>
+<container id="footer_" class="container sticky-bottom my-auto">
+    <footer class="pt-4 mt-4 footer mt-auto sticky-bottom">
+
+
+
+        <p class="text-center text-muted ">
+            <!--hidden when full width -->
+            © 2022 IHI asso
+        </p>
+
+    </footer>
+
+</container>
+</container_main>
+</body>
+<script type='text/javascript' src='css_js/js/popper.min.js'></script>
+<script type='text/javascript' src='css_js/js/bootstrap.js'></script>
+<script type='text/javascript' src='css_js/js/main.js'></script>
+
+</html>
---
+++ y_xlau/admiss.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>IHI</title>
+
+    <!-- <link rel="stylesheet" type="text/css" href="css_js/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css_js/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/> -->
+    <style type="text/css">
+
+</style>
+    <link rel="stylesheet" type="text/css" href="../css_js/expln°style.css" />
+
+
+</head>
+
+<body class="mb-0">
+
+
+
+    <container_main class="container">
+
+    <container class="auto-mx ">
+
+<div id="" class=" d-flex align-items-center justify-content-center">
+    <?php 
+    include_once "4. Admissibilité à l'aide.html"; 
+    ?>
+</div>
+
+</container>
+
+<container class="auto-mx hid_btn_xl">
+<!-- 
+<article class="article">
+    <div class="container px-4 py-5">
+        <div id="featured" class="d-flex align-items-center justify-content-center">
+            <a id="a_​_Voir_" href="y_storage/Règlement sur les bénévoles.pdf"><div class="T2 Tcustom">Voir+</div></a>
+            <h1 class="T4 Tcustom pb-2 border-bottom ">​ Dons faits au profit d'organismes étrangers   </h1>
+        
+        <div class="">
+            <div class="wrapper ">
+                <p>
+                
+    
+
+
+                </p>
+            </div>
+        </div>
+    </div>
+</article> -->
+
+     
+     
+     <!-- end -->
+
+
+
+</container>
+
+</div>
+
+
+
+<!-- footer -->
+<span class="spacer">
+</span>
+<container id="footer_" class="container sticky-bottom my-auto">
+    <footer class="pt-4 mt-4 footer mt-auto sticky-bottom">
+
+
+
+        <p class="text-center text-muted ">
+            <!--hidden when full width -->
+            © 2022 IHI asso
+        </p>
+
+    </footer>
+
+</container>
+</container_main>
+</body>
+<script type='text/javascript' src='css_js/js/popper.min.js'></script>
+<script type='text/javascript' src='css_js/js/bootstrap.js'></script>
+<script type='text/javascript' src='css_js/js/main.js'></script>
+
+</html>
