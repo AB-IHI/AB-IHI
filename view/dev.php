fcd7ce2221a264b11d187ca40f6b8b76b8c9e263
John Doe <timour1712@gmail.com>
2022-10-14 16:13:41 +0200

    return

---
+++ view/dev.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cette fonctionnalité est en developpement</h1>
+    <a type="submit" class="cnx-sub btn_dem btn-lg mx-auto btn-outline-dark"  href="?page=home"value="connexion">Retour</a>
+</body>
+</html>
