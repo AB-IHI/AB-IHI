fc3402405c8c64e17af34275c9a82c9c3d7d13d2
tim <timour1712@gmail.com>
2022-12-19 18:25:39 +0100

    ubral poslednii doubloni i 450 visoti teper

--- view/home_2.php
+++ view/home_2.php
@@ -341,7 +341,7 @@
                     $path = "y_ymages/photos";
                     foreach(glob($path.'/*.*') as $file) {
                         echo '<div class="carousel-item ">
-                            <img src="' . $file . '" class="d-block w-100 c_img" alt="photo refugee">
+                            <img src="' . $file . '" class="d-block <!--w-100--> c_img" alt="photo refugee">
                         </div>';
                     }
                     ?>
